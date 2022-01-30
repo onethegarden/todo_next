0386b466e39db7f358bde6de3470b21e51f0394b
Jeongwon Han <onethegarden@gmail.com>
2022-01-30 19:20:27 +0900

    todos api route로 변경 (#4)

--- src/api/todo.ts
+++ src/api/todo.ts
@@ -1,23 +1,11 @@
 import axios from "axios";
 import { Todo } from "../model/todo";
 
-export const BASE_URI = "http://localhost:4000";
+export const BASE_URI = "http://localhost:3000";
 
 const todoAPI = {
   todoList: async (): Promise<Todo[]> => {
-    return (await axios.get<Todo[]>(`${BASE_URI}/todo`)).data;
-  },
-  todoInsert: async (todo: { content: string }): Promise<Todo> => {
-    return (await axios.post<Todo>(`${BASE_URI}/todo`, todo)).data;
-  },
-  todoToggle: async (todoId: number): Promise<Todo> => {
-    return (await axios.patch<Todo>(`${BASE_URI}/todo/${todoId}`)).data;
-  },
-  todoPut: async (todo: Todo): Promise<Todo> => {
-    return (await axios.put<Todo>(`${BASE_URI}/todo/${todo.id}`, todo)).data;
-  },
-  todoDelete: async (todoId: number): Promise<Todo> => {
-    return (await axios.delete<Todo>(`${BASE_URI}/todo/${todoId}`)).data;
+    return (await axios.get<Todo[]>(`${BASE_URI}/api/todo`)).data;
   },
 };
 
